8df614c5af8e8a604b45a049265fd0b62ff14e60
Helen Cheng <meicheng96@gmail.com>
2014-11-15 01:53:59 -0800

    something

---
+++ main.py
@@ -0,0 +1,34 @@
+import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = jinja_environment.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
+
+
+
+#import webapp2
+#import jinja2
+#import os
+
+#jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+#class MainHandler(webapp2.RequestHandler):
+#    def get(self):
+#        template_values = {}
+#        template = jinja_environment.get_template('index.html')
+#        self.response.write(template.render(template_values))
+
+#app = webapp2.WSGIApplication([
+#    ('/', MainHandler)
+#], debug=True)
